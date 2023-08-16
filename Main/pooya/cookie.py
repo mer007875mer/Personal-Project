31bd0ce14ea3558c3fbefa06b77792d1b35647ae
Erfan Hooman <er.callof.007875@gmail.com>
2023-08-16 16:08:23 +0330

    A3

--- Main/pooya/cookie.py
+++ Main/pooya/cookie.py
@@ -2,12 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import json
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
-def collectcookie():
-    chrome_driver_path = '/home/erfan-hooman/Desktop/Personal-Project/Main/pooya/chromedriver'
+def collect_cookie(name):
+    chrome_driver_path = f'/home/{name}/Desktop/Personal-Project/Main/pooya/chromedriver'
     service = Service(chrome_driver_path)
 
     driver = webdriver.Chrome(service=service)
@@ -16,36 +15,69 @@ def collectcookie():
     gov_button = driver.find_element(By.CLASS_NAME, "ssologin")
     gov_button.click()
 
-    number = "09037665074"
-    captcha = input("Enter the captcha: ")
+    try:
+        start_with_cookies(name)
+    except Exception as e:
+        print(e)
+        number = "09037665074"
+        captcha = input("Enter the captcha: ")
 
-    number_input = driver.find_element(By.ID, "username")
-    number_input.send_keys(number)
+        number_input = driver.find_element(By.ID, "username")
+        number_input.send_keys(number)
 
-    captcha_input = driver.find_element(by='css selector', value='.captcha_section > :nth-child(2) > .form-control')
-    captcha_input.send_keys(captcha)
+        captcha_input = driver.find_element(by='css selector', value='.captcha_section > :nth-child(2) > .form-control')
+        captcha_input.send_keys(captcha)
 
-    send_button = driver.find_element(By.ID, "send-otp-form-btn")
-    send_button.click()
+        send_button = driver.find_element(By.ID, "send-otp-form-btn")
+        send_button.click()
 
-    number1 = driver.find_element(By.CSS_SELECTOR, ".first")
-    number2 = driver.find_element(By.CSS_SELECTOR, ".second")
-    number3 = driver.find_element(By.CSS_SELECTOR, ".third")
-    number4 = driver.find_element(By.CSS_SELECTOR, ".fourth")
-    number5 = driver.find_element(By.CSS_SELECTOR, ".fifth")
+        number1 = driver.find_element(By.CSS_SELECTOR, ".first")
+        number2 = driver.find_element(By.CSS_SELECTOR, ".second")
+        number3 = driver.find_element(By.CSS_SELECTOR, ".third")
+        number4 = driver.find_element(By.CSS_SELECTOR, ".fourth")
+        number5 = driver.find_element(By.CSS_SELECTOR, ".fifth")
 
-    number1.send_keys(input("enter 1 number: "))
-    number2.send_keys(input("enter 2 number: "))
-    number3.send_keys(input("enter 3 number: "))
-    number4.send_keys(input("enter 4 number: "))
-    number5.send_keys(input("enter 5 number: "))
+        number1.send_keys(input("enter 1 number: "))
+        number2.send_keys(input("enter 2 number: "))
+        number3.send_keys(input("enter 3 number: "))
+        number4.send_keys(input("enter 4 number: "))
+        number5.send_keys(input("enter 5 number: "))
 
-    l_button = driver.find_element(By.ID, "submitLogin")
-    l_button.click()
+        l_button = driver.find_element(By.ID, "submitLogin")
+        l_button.click()
 
-    cookies = driver.get_cookies()
+        time.sleep(30)
 
-    with open('cookies.json', 'w') as f:
-        json.dump(cookies, f)
+        open_button = driver.find_element(By.CSS_SELECTOR, "body > div > form > button")
+        open_button.click()
 
-    driver.quit()
+        cookies = driver.get_cookies()
+
+        with open('cookies.json', 'w') as f:
+            json.dump(cookies, f)
+
+        time.sleep(30)
+
+        driver.quit()
+
+
+def start_with_cookies(name):
+    chrome_driver_path = f'/home/{name}/Desktop/Personal-Project/Main/pooya/chromedriver'
+    service = Service(chrome_driver_path)
+
+    driver = webdriver.Chrome(service=service)
+
+    with open('cookies.json', 'r') as f:
+        cookies = json.load(f)
+
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+    driver.get("https://pooya.kashanu.ac.ir/gateway/PuyaAuthenticate.php")
+
+    time.sleep(15)
+
+    open_button = driver.find_element(By.CSS_SELECTOR, "body > div > form > button")
+    open_button.click()
+
+    time.sleep(15)
--- Main/pooya/main.py
+++ Main/pooya/main.py
@@ -1,27 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from cookie import collectcookie
+from cookie import collect_cookie, start_with_cookies
+import os
 
-chrome_driver_path = '/home/erfan-hooman/Desktop/Personal-Project/Main/pooya/chromedriver'
+name = os.getenv('USER')
+
+chrome_driver_path = f'/home/{name}/Desktop/Personal-Project/Main/pooya/chromedriver'
 service = Service(chrome_driver_path)
-
-collectcookie()
-
-# driver = webdriver.Chrome(service=service)
-# driver.get("https://pooya.kashanu.ac.ir/gateway/PuyaAuthenticate.php")
-#
-# username = "40121160031"
-# password = "Mer@@7875"
-#
-# username_input = driver.find_element(By.ID, 'UserID')
-# password_input = driver.find_element(By.ID, 'DummyVar')
-# captcha_input = driver.find_element(By.ID, 'capt')
-#
-# username_input.send_keys(username)
-# password_input.send_keys(password)
-# captcha = input("Please enter the captcha value: ")
-# captcha_input.send_keys(captcha)
-#
-# login_button = driver.find_element(By.CLASS_NAME, 'login100-form-btn')
-# login_button.click()
+print(name)
+collect_cookie(name)
