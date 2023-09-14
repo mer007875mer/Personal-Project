57e048f9e9b62de248b24f920703b1ff7f42c08b
Erfan Hooman <er.callof.007875@gmail.com>
2023-09-14 16:51:21 +0330

    13-sep

--- WE/djnago/Divar/Divar/urls.py
+++ WE/djnago/Divar/Divar/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from Main import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.ScrapedApartmentsView.as_view(), name="scraped_apartments"),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
